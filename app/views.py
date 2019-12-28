34a7468e94b4eb5a645bf8a76c454bc8514c01e4
Tung Duong Thanh <tung.duong.thanh@ntq-solution.com.vn>
2019-12-28 18:33:44 +0700

    Update

--- app/views.py
+++ app/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render, redirect
 from . import db
+import os
+from django.core.files.storage import FileSystemStorage
+
+def saveFile(studentNo, file):
+    path = 'static/' + studentNo + '.jpg'
+    if os.path.exists(path):
+        os.remove(path)
+    fs = FileSystemStorage()
+    fs.save(path, file)
+
+import time
 
 def index(request):
-    context = {'students' : db.getAllStudents()}
+    context = {'students' : db.getAllStudents(), 
+                'time': time.time()}
     return render(request, 'index.html', context)
 
 def createStudent(request):
     if request.method == 'POST':
         studentNo = request.POST['studentNo']
         name = request.POST['name']
-        db.createStudent(studentNo, name)
+        db.createStudent(studentNo, name)        
+        file = request.FILES.get('image')
+        if file and file.name:
+            saveFile(studentNo, file)
         return redirect('home')
 
     return render(request, 'student_form.html')
-
 
 def updateStudent(request, id):
     st = db.getStudentById(id)
@@ -23,6 +37,9 @@ def updateStudent(request, id):
         studentNo = request.POST['studentNo']
         name = request.POST['name']
         db.updateStudent(id, studentNo, name)
+        file = request.FILES.get('image')
+        if file and file.name:
+            saveFile(studentNo, file)
         return redirect('home')
 
     return render(request, 'student_form.html', 
